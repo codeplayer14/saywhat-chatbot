714dd32d7eb296c9bdc491ed7ba22a38b10b11f8
Padmanabh Gupta <padmanabh14@gmail.com>
2018-09-16 23:48:18 +0530

    Created Encoder layer

--- chatbot.py
+++ chatbot.py
@@ -142,10 +142,22 @@ def model_inputs():
     lr = tf.placeholder(tf.float32,name='learning_rate')
     keep_prob = tf.placeholder(tf.placeholder,name='keep_prob')
     return inputs,targets,lr,keep_prob
+    
+    def preprocess_targets(targets,word2int,batch_size=10):
+        left_side = tf.fill([batch_size,1],word2int['<SOS>'])
+        right_side = tf.strided_slice(targets,0,[batch_size,-1],[1,1])
+        preprocessed_targets = tf.concat((left_side,right_side),axis=1)
+        return preprocessed_targets
 
-def preprocess_targets(targets,word2int,batch_size=10):
-    left_side = tf.fill([batch_size,1],word2int['<SOS>'])
-    right_side = tf.strided_slice(targets,0,[batch_size,-1],[1,1])
-    preprocessed_targets = tf.concat((left_side,right_side),axis=1)
-    return preprocessed_targets
-
+#RNN layer - Encoder
+        
+def encoder_rnn_layer(rnn_inputs,rnn_size,num_layers,keep_prob,sequence_length):
+    lstm = tf.contrib.rnn.BasicLSTMCell(rnn_size)   
+    lstm_dropout = tf.contrib.rnn.DropoutWrapper(lstm,input_keep_prob=keep_prob)
+    encoder_cell = tf.contrib.rnn.MultiRNNCell([lstm_dropout]*num_layers)
+    _,encoder_state = tf.nn.bidirectional_dynamic_rnn(cell_fw = encoder_cell,
+                                                      cell_bw = encoder_cell,
+                                                      sequence_length=sequence_length,
+                                                      inputs = rnn_inputs,
+                                                      dtype=tf.float32)
+    return encoder_state
